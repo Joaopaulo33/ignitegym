cd93e032a16c6ff6b9476e5efa6cf462d15bad46
joaopaulo33 <joaopaulo33573@gmail.com>
2024-05-13 06:52:09 -0300

    Feat: Utilizando variáveis para definir o tipo do botão

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,24 +1,45 @@
-import { Button as ButtonNativeBase, IButtonProps, Text } from "native-base";
+import { VStack, Image, Text, Center, Heading } from "native-base";
 
-type Props = IButtonProps & {
-  title: string;
-};
+import LogoSvg from "@assets/logo.svg";
+import BackgroundImg from "@assets/background.png";
 
-export function Button({ title, ...rest }: Props) {
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+export function SignIn() {
   return (
-    <ButtonNativeBase
-      w="full"
-      h={14}
-      bg="green.700"
-      rounded="sm"
-      _pressed={{
-        bg: "green.500",
-      }}
-      {...rest}
-    >
-      <Text color="white" fontFamily="heading" fontSize="sm">
-        {title}
-      </Text>
-    </ButtonNativeBase>
+    <VStack flex={1} bg="gray.700" px={10}>
+      <Image
+        source={BackgroundImg}
+        alt="Pessoas treinando"
+        resizeMode="contain"
+        position="absolute"
+      />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo.
+        </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse a conta
+        </Heading>
+
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Senha" secureTextEntry />
+
+        <Button title="Acessar" />
+      </Center>
+
+      <Button title="Criar Conta" variant="outline" />
+    </VStack>
   );
 }
